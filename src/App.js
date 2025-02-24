5b926350cc844d9e94626b08d548194ff8f60432
shauntabs <shauntaberer@gmail.com>
2025-02-23 19:36:32 -0500

    Initial commit - Hello World React app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello, World!</h1>
+      <p>My first React app is working!</p>
+    </div>
+  );
+}
+
+export default App;
